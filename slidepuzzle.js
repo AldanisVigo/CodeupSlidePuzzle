c72c5383f919662c20c34eb66e92421c7d6db774
Aldanis Vigo <aldanisvigo@gmail.com>
2022-10-02 03:55:18 -0500

    changed to index.html

--- slidepuzzle.js
+++ slidepuzzle.js
@@ -1,28 +1,38 @@
 let slots = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 slots.push(null)
-console.log(slots)
+//Background music fx from : https://mixkit.co/free-sound-effects/music/ , name: Synth suspense music
+let backgroundMusic = new Audio('backgroundmusic.wav')
+backgroundMusic.volume = 0.2
+let backgroundMusicPlaying = false
+let backgroundMusicControlContainer = document.createElement('div')
+let backgroundMusicPausePlay = document.createElement('button')
+backgroundMusicPausePlay.innerText = 'MUTE MUSIC'
+backgroundMusicPausePlay.onclick = e => {
+    if(e.target.innerText == 'PLAY MUSIC'){
+        e.target.innerText = 'MUTE MUSIC'
+        backgroundMusic.play()
+    }else if(e.target.innerText == 'MUTE MUSIC'){
+        e.target.innerText = 'PLAY MUSIC'
+        backgroundMusic.pause()
+    }
+}
 
+backgroundMusicControlContainer.classList.add('background_music_controls')
+backgroundMusicControlContainer.appendChild(backgroundMusicPausePlay)
 const canSwap = (indexa,indexb) => {
     if(slots[indexa].innerText !== '' && slots[indexb].innerText !== '') return false
-
-    // if(Math.abs(indexa-indexb) === 4) return true
-    // if(Math.abs(indexa-indexb) === 1) return true
     let allowSwap = false   
     let aleft = Number(slots[indexa].style.left.split('px')[0])
     let atop = Number(slots[indexa].style.top.split('px')[0])
-
     let bleft = Number(slots[indexb].style.left.split('px')[0])
     let btop = Number(slots[indexb].style.top.split('px')[0])
-
     if(Math.abs(aleft - bleft) == 100 && atop == btop){
         allowSwap = true
     }
-
     if(Math.abs(btop - atop) == 100 && aleft == bleft){
     
         allowSwap = true
     }
-
     return allowSwap
 }
 
@@ -38,7 +48,7 @@ function getRandomInt(min, max) {
     Let's think about how shuffling a slide puzzle works. When we begin
         1. We find the empty slot
         2. Decide which pice we want to fill the empty slot, only choosing from the adjacent pieces that are perpendicular to the empty slot
-        3. Pick the pice and swap with it
+        3. Pick the piece and swap with it
         4. Do this n times to "shuffle" the puzzle.
 
     This is analogous to doing it by hand.
@@ -56,161 +66,87 @@ const shuffleTruffle = (board) => {
         if(emptySlotIndex + 4 <= 15){
             adjacent.push(emptySlotIndex + 4)
         }
-        
         if(emptySlotIndex - 1 >= 0){
             adjacent.push(emptySlotIndex - 1)
         }
         if(emptySlotIndex + 1 <= 15){
             adjacent.push(emptySlotIndex + 1)
         }
-
         //choose a random one in the array
         let randomAdjacent = adjacent[getRandomInt(0,adjacent.length)]
-        
         //swap with the emptySlotIndex 
         arr[emptySlotIndex] = arr[randomAdjacent]
         arr[randomAdjacent] = null
-
         //update the emptySlotIndex
         emptySlotIndex = randomAdjacent
     }
-    console.log(arr)
     return arr
 }
 
 
+const checkWin = () => {
+    let currentSlots = document.getElementsByClassName('slot')
+
+    for(let i = 0; i < slots.length; i++){
+
+    }
+}
+
 let selectedSlot = null
 slots = shuffleTruffle(slots)
-
 slots.forEach((slot,index)=>{
-    
     let newSlot = document.createElement('div')
     if(slot !== null){
         newSlot.classList.add('slot')
-
         // calculate y position
         let t = Math.floor(index / 4) * 100
         //calculate x position 
         let l = (index % 4) * 100
-
         newSlot.style.position = 'absolute'
         newSlot.style.left = l + 'px'
         newSlot.style.top = t + 'px'
-
         document.getElementById('container').appendChild(newSlot)
     }else{
         newSlot.classList.add('empty_slot')
-                
         //calculate y position
         let t = Math.floor(index / 4) * 100 
         //calculate y position 
         let l = (index % 4) * 100 
-
         newSlot.style.position = 'absolute'
         newSlot.style.left = l + 'px'
         newSlot.style.top = t + 'px'
-
         document.getElementById('container').appendChild(newSlot)
     }
     newSlot.innerText = slot
-
     slots[index] = newSlot
-
     newSlot.onclick = () => {
+        //Start background music on first move, we need a user click for sound
+        if(!backgroundMusicPlaying){
+            backgroundMusic.play()
+            backgroundMusic.loop = true
+            backgroundMusicPlaying = true
+            document.body.appendChild(backgroundMusicControlContainer)
+        }
         if(selectedSlot == null){
             selectedSlot = newSlot
         }else{
             let indexOfSelected = slots.indexOf(selectedSlot)
             let indexOfCurrent = slots.indexOf(newSlot)
-
             let swp = canSwap(indexOfSelected,indexOfCurrent) ?? false
-            console.log("Can swap : " + swp)
-
             if(swp){
                 let selectedLeft = slots[indexOfSelected].style.left
                 let selectedTop = slots[indexOfSelected].style.top
-
                 let currentLeft = slots[indexOfCurrent].style.left
                 let currentTop = slots[indexOfCurrent].style.top
-
                 slots[indexOfSelected].style.left = currentLeft
                 slots[indexOfSelected].style.top = currentTop
-
                 newSlot.style.left = selectedLeft
                 newSlot.style.top = selectedTop
+                //Slide sound fx from : https://mixkit.co/free-sound-effects/slide/ , name: Cabinet Door Slide
+                let slideEffect = new Audio('slide.wav')
+                slideEffect.play()
             }
-
             selectedSlot = null
         }
     }
 })
-
-
-
-//Swap Method 
-// const init = () => {
-//     Array.from(slot_elems).forEach((slot_box,index)=>{
-//         slot_box.innerText = slots[index] ?? ''
-//         console.log(slot_box)
-//         console.log(index)
-//         slot_box.onclick = e => {
-//             let contents = e.target.innerHTML
-//             console.log("Selected box" + selectedBox)
-//             console.log("Contents of clicked box: " + contents)
-//             if(selectedBox != null && contents === '' && canSwap(index,selectedBox)){
-//                 console.log("Swapping")
-//                 // let selectedStyle = window.getComputedStyle(slot_elems[selectedBox])
-//                 // let selectedBoxLeft = selectedStyle.left
-//                 // let selectedBoxTop = selectedStyle.top
-
-//                 // let emptySlotStyle = window.getComputedStyle(slot_elems[index])
-//                 // let emptySlotLeft = emptySlotStyle.left
-//                 // let emptySlotTop = emptySlotStyle.top
-
-//                 // slot_elems[index].style.left = selectedBoxLeft
-//                 // slot_elems[index].style.top = selectedBoxTop
-
-//                 let selectedRect = slot_elems[selectedBox].getBoundingClientRect()
-//                 let selectedBoxLeft = selectedRect.left
-//                 let selectedBoxTop = selectedRect.top
-
-//                 let emptySlotRect = slot_elems[index].getBoundingClientRect()
-//                 let emptySlotLeft = emptySlotRect.left
-//                 let emptySlotTop = emptySlotRect.top
-
-//                 let selectedBoxTransform, emptySlotTransform
-//                 if(selectedBox < index){
-//                     selectedBoxTransform = 'translateX(' + Number(Number(emptySlotLeft) - Number(selectedBoxLeft)) + 'px) translateY(' + Number(Number(emptySlotTop) - Number(selectedBoxTop)) + 'px)'
-//                     console.log(selectedBoxTransform)
-
-//                     emptySlotTransform = 'translateX(' + Number(Number(selectedBoxLeft) - Number(emptySlotLeft)) + 'px) translateY(' + Number(Number(selectedBoxTop) - Number(emptySlotTop))  +'px)'
-//                     console.log(emptySlotTransform)
-//                 }else{
-//                     selectedBoxTransform = 'translateX(' + Number(Number(selectedBoxLeft) - Number(emptySlotLeft)) + 'px) translateY(' + Number(Number(selectedBoxTop) - Number(emptySlotTop)) + 'px)'
-//                     console.log(selectedBoxTransform)
-
-//                     emptySlotTransform = 'translateX(' + Number(Number(emptySlotLeft) - Number(selectedBoxLeft)) + 'px) translateY(' + Number(Number(emptySlotTop) - Number(selectedBoxTop))  +'px)'
-//                     console.log(emptySlotTransform)
-//                 }
-
-//                 slot_elems[selectedBox].style.transform = selectedBoxTransform
-//                 slot_elems[index].style.transform = emptySlotTransform
-
-//                 //perform swap in array
-//                 let selectedCpy = Object.assign({},slot_elems[selectedBox])
-//                 let emptyCpy = Object.assign({},slot_elems[index])
-//                 slot_elems[selectedBox] = emptyCpy
-//                 slot_elems[index] = selectedCpy
-
-//                 selectedBox = null
-//             }else{
-//                 unselectAllBoxes()
-//                 slot_elems[index].classList.add('selected')
-//                 selectedBox = index
-//             }
-//         }
-//     })
-// }
-
-// init()
-
